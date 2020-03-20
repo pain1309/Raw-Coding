15178082625a68cbc5b3deb1f30378784aae9ee9
vananh1309 <anhvv13@gmail.com>
2020-03-20 16:53:37 +0700

    2003

---
+++ Authentication/Server/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Constants
+    {
+        public const string Issuer = Audience;
+        public const string Audience = "https://localhost:44323/";
+        public const string Secret = "not_too_short_secret_otherwise_it_might_error";
+    }
+}
